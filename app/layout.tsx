7dadfcab71f13eb77d84a5b577d944ae86715515
zidanhafiz <nadizsonata@gmail.com>
2025-01-23 21:31:08 +0800

    Change base theme to light

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
       <body className={`${inter.className} antialiased`}>
         <ThemeProvider
           attribute="class"
-          defaultTheme="system"
+          defaultTheme="light"
           enableSystem
           disableTransitionOnChange
         >
