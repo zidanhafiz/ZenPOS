3e08d5bfb1b3128100990a98400577c18b96fbc4
zidanhafiz <nadizsonata@gmail.com>
2025-02-11 22:26:53 +0700

    Fix separator size for PageHeading component

--- components/PageHeading.tsx
+++ components/PageHeading.tsx
@@ -14,7 +14,7 @@ export default function PageHeading({
       <div className="flex gap-2 items-center">
         <SidebarTrigger />
         <h4 className="text-lg font-semibold">{title}</h4>
-        <Separator orientation="vertical" />
+        <Separator orientation="vertical" className="h-4" />
         <p className="text-xs md:text-sm text-gray-500">{description}</p>
       </div>
       <CartToggleButton />
